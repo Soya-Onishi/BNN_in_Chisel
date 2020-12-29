a3a5ef6bb58dbddd052c8e04ad8030080073e2de
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-12-29 12:03:12 +0900

    Fix BinaryActivation and for Convolution.
    For now, there is no summarized buffer to adjust output size

--- src/main/scala/bnn/BinaryActivation.scala
+++ src/main/scala/bnn/BinaryActivation.scala
@@ -3,6 +3,31 @@ package bnn
 import chisel3._
 import chisel3.util._
 
+class BinaryActivation2(
+  val size: Int,
+  val inputWidth: Int,
+  val biases: Seq[Int]
+) extends Module {
+  val io = IO(new Bundle {
+    val inData = Flipped(DecoupledIO(Vec(size, UInt(inputWidth.W))))
+    val outData = DecoupledIO(Vec(size, Bool()))
+  })
+  val biasIdxMax = math.ceil(biases.length.toFloat / size.toFloat).toInt - 1
+  val biasWidth = unsignedBitLength(biases.max)
+
+  val (biasCounter, biasIdx) = DeluxeCounter(biasIdxMax)
+  val biasess = VecInit(biases.map(_.U(biasWidth.W)).sliding(size, size).map(v => VecInit(v)).toSeq)
+  val activateds = (io.inData.bits zip biasess(biasIdx.current)).map{ case (v, bias) => v > bias }
+
+  when(io.inData.valid && io.outData.ready) {
+    biasCounter.count()
+  }
+
+  io.outData.bits := VecInit(activateds)
+  io.outData.valid := io.inData.valid
+  io.inData.ready := io.outData.ready
+}
+
 abstract class BinaryActivation[InputType <: Data, OutputType <: Data](
   channels: Int,
   inputSize: Int,
--- src/main/scala/bnn/BinaryActivationConv2D.scala
+++ src/main/scala/bnn/BinaryActivationConv2D.scala
@@ -1,7 +1,53 @@
 package bnn
 
 import chisel3._
+import chisel3.util._
 
+class BinaryActivationConv2D(
+  val size: Int,
+  val inputWidth: Int,
+  val biases: Seq[Int]
+) extends BinaryLayer {
+  val io = IO(new Bundle {
+    val inData = Flipped(DecoupledIO(Pixel(Vec(size, UInt(inputWidth.W)))))
+    val outData = DecoupledIO(Pixel(Vec(size, Bool())))
+  })
+
+  val inValid = RegInit(false.B)
+  val inBits = Reg(Pixel(Vec(size, UInt(inputWidth.W))))
+
+  val outValid = RegInit(false.B)
+  val outBits = Reg(Pixel(Vec(size, Bool())))
+
+  val activation = Module(new BinaryActivation2(size, inputWidth, biases))
+
+  // ready signal is used here to determine whether renew registers
+  // and this signal is passed to previous layer(i.e. BinaryConv2D)
+  io.inData.ready := io.outData.ready
+  when(io.outData.ready) {
+    inBits  := io.inData.bits
+    inValid := io.inData.valid
+
+    outBits.bits        := activation.io.outData.bits
+    outBits.topLeft     := inBits.topLeft
+    outBits.bottomRight := inBits.bottomRight
+    outBits.left        := inBits.left
+    outBits.right       := inBits.right
+    outBits.valid       := inBits.valid
+    outValid            := activation.io.outData.valid
+  }
+
+  // From source
+  activation.io.inData.valid := inValid
+  activation.io.inData.bits  := inBits.bits
+
+  activation.io.outData.ready := io.outData.ready
+  io.outData.valid := outValid
+  io.outData.bits  := outBits
+}
+
+
+/*
 class BinaryActivationConv2D(
   val channels: Int,
   val inputSize: Int,
@@ -15,6 +61,8 @@ class BinaryActivationConv2D(
   Pixel(Vec(inputSize, UInt(inputWidth.W))),
   Pixel(Vec(channels, Bool()))
 ) {
+
+
   val isLeft        = Reg(Bool())
   val isRight       = Reg(Bool())
   val isTopLeft     = Reg(Bool())
@@ -44,3 +92,4 @@ class BinaryActivationConv2D(
     }
   }
 }
+*/
