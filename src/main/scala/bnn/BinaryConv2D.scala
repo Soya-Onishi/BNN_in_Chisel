b7c1dacaa7e954876504037cedaa83dfbaef3a6b
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-12-30 22:25:54 +0900

    Add BinaryConv2DUInt layer
    This have not been tested yet

--- src/main/scala/bnn/BinaryConv2D.scala
+++ src/main/scala/bnn/BinaryConv2D.scala
@@ -4,6 +4,8 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 
+abstract class BinaryConv2D extends BinaryLayer
+
 @chiselName
 class BinaryConv2DBinary(
   kernelSize: (Int, Int),
@@ -12,7 +14,7 @@ class BinaryConv2DBinary(
   inputShape: (Int, Int, Int),
   cyclesForAllWeights: Int,
   stride: Int
-) extends Module {
+) extends BinaryConv2D {
   val (kernelH, kernelW) = kernelSize
   val (inputH, inputW, inputC) = inputShape
   val weightsLength = kernelH * kernelW
@@ -84,6 +86,119 @@ class BinaryConv2DBinary(
       weightIdxCounter.count()
       when(weightIdx.wrapped) {
         layerBase.io.outData.ready := true.B
+      }
+    }
+  }
+}
+
+@chiselName
+class BinaryConv2DUInt (
+  kernelSize: (Int, Int),
+  weightss: Seq[Seq[Boolean]],
+  inputSize: Int,
+  inputShape: (Int, Int, Int),
+  cyclesForAllWeights: Int,
+  pixelsPerCycle: Int,
+  stride: Int,
+  inWidth: Int
+) extends BinaryConv2D {
+  val (kernelH, kernelW) = kernelSize
+  val (inputH, inputW, inputC) = inputShape
+  val weightsLength = kernelH * kernelW
+
+  val weightsPerCycle = math.ceil(weightss.head.length.toFloat / cyclesForAllWeights.toFloat).toInt
+  val weightCounts = math.ceil(weightss.head.length.toFloat / weightsPerCycle.toFloat).toInt
+  val weightIdxMax = weightCounts - 1
+
+  val pixelCounts = math.ceil((kernelH * kernelW).toFloat / pixelsPerCycle.toFloat).toInt
+  val pixelIdxMax = pixelCounts - 1
+
+  val outputSize = weightsPerCycle
+  val bitWidth = weightss.head.length * inputC
+  val outUIntWidth = unsignedBitLength(bitWidth) + inWidth
+
+  val weightssPad = (weightss ++ Seq.fill(weightss.length % weightsPerCycle)(Seq.fill(weightsLength)(false))).map {
+    weights => weights ++ Seq.fill(weights.length % pixelsPerCycle)(false)
+  }
+  val weightssss = weightssPad.map(_.sliding(pixelsPerCycle, pixelsPerCycle).toVector).sliding(weightsPerCycle, weightsPerCycle).toVector.map(_.transpose)
+
+  assert(inputC >= inputSize)
+  assert(weightss.nonEmpty)
+  assert(weightss.head.length == weightsLength)
+
+  val io = IO(new Bundle {
+    val inData = Flipped(DecoupledIO(Pixel(Vec(inputSize, UInt(inWidth.W)))))
+    val outData = DecoupledIO(Pixel(Vec(outputSize, SInt(outUIntWidth.W))))
+
+    val isInit = Output(Bool())
+  })
+
+  val (weightIdxCounter, weightIdx) = DeluxeCounter(weightIdxMax)
+  val (pixelIdxCounter, pixelIdx) = DeluxeCounter(pixelIdxMax)
+  val (channelIdxCounter, channelIdx) = DeluxeCounter(inputC - 1)
+  val layerBase = Module(new Binary2DLayer(kernelSize, inputSize, inputShape, stride, UInt(inWidth.W)))
+  val bitCounters = Seq.fill(weightsPerCycle)(BitCounter(bitWidth))
+  val convTmpBuffers = RegInit(VecInit(Seq.fill(weightsPerCycle)(0.S(outUIntWidth.W))))
+
+  layerBase.io.inData <> io.inData
+  layerBase.io.outData.ready := false.B
+
+  io.outData.bits             := DontCare
+  io.outData.valid            := false.B
+  io.outData.bits.left        := layerBase.io.isLeft
+  io.outData.bits.right       := layerBase.io.isRight
+  io.outData.bits.topLeft     := layerBase.io.isTopLeft
+  io.outData.bits.bottomRight := layerBase.io.isBottomRight
+  io.outData.bits.valid       := false.B
+  io.isInit                   := layerBase.io.isInit
+
+  when(layerBase.io.outData.valid) {
+    val defaultWeightss = VecInit(Seq.fill(weightCounts)(VecInit(Seq.fill(pixelCounts)(false.B))))
+    val weightss = MuxLookup(weightIdx.current, defaultWeightss, weightssss.zipWithIndex.map {
+      case (weightsss, idx) =>
+        val i = idx.U(unsignedBitLength(weightIdxMax).max(1).W)
+        i -> MuxLookup(pixelIdx.current, defaultWeightss, weightsss.zipWithIndex.map {
+          case (weightss, idx) =>
+            val bitss = VecInit(weightss.map(weights => VecInit(weights.map(_.B))))
+            idx.U(unsignedBitLength(pixelIdxMax).max(1).W) -> bitss
+        })
+    })
+
+    val pixelss = layerBase.io.outData.bits.sliding(pixelsPerCycle, pixelsPerCycle).toVector
+    val pixels = MuxLookup(pixelIdx.current, VecInit(Seq.fill(weightsPerCycle)(0.U(inWidth.W))), pixelss.zipWithIndex.map {
+      case (pixels, idx) =>
+        val bits = pixels.map{ p => p.bits(channelIdx.current) }
+        val padding = Seq.fill(weightsPerCycle - pixels.length)(0.U(inWidth.W))
+        val appliedPixelBits = bits ++ padding
+
+        idx.U(unsignedBitLength(pixelIdxMax).max(1).W) -> VecInit(appliedPixelBits)
+    })
+
+    val weighteds = (convTmpBuffers zip weightss).map {
+      case (buf, weights) =>
+        val sintPixels = pixels.map(_.pad(outUIntWidth)).map(_.asSInt())
+        (weights zip sintPixels).map{ case (w, p) => Mux(w, p, -p) }.foldLeft(buf)(_ + _)
+    }
+
+    io.outData.valid      := weightIdx.wrapped & pixelIdx.wrapped & channelIdx.wrapped
+    io.outData.bits.valid := true.B
+    io.outData.bits.bits  := VecInit(weighteds)
+
+    convTmpBuffers := MuxCase(VecInit(weighteds), Seq(
+      channelIdx.wrapped & pixelIdx.wrapped & weightIdx.wrapped & io.outData.ready -> 0.U,
+      channelIdx.wrapped & pixelIdx.wrapped & weightIdx.wrapped                    -> convTmpBuffers,
+    ))
+
+    when(!(channelIdx.wrapped & pixelIdx.wrapped & weightIdx.wrapped & !io.outData.ready)) {
+      channelIdxCounter.count()
+      when(channelIdx.wrapped) {
+        pixelIdxCounter.count()
+        when(pixelIdx.wrapped) {
+          weightIdxCounter.count()
+          when(weightIdx.wrapped) {
+            layerBase.io.outData.ready := true.B
+          }
+        }
       }
     }
   }
