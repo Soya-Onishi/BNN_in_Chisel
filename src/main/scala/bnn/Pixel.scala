1cada6a1ed4093181404a9515f17192bffa8747e
SoyaOhnishi <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2020-12-31 14:42:57 +0900

    Add invalid pixel generator

--- src/main/scala/bnn/Pixel.scala
+++ src/main/scala/bnn/Pixel.scala
@@ -15,4 +15,15 @@ class Pixel[T <: Data](gen: T) extends Bundle {
 
 object Pixel {
   def apply[T <: Data](gen: T) = new Pixel[T](gen)
+  def invalid[T <: Data](gen: T): Pixel[T] = {
+    val p = Wire(new Pixel[T](gen))
+    p.bits        := DontCare
+    p.valid       := false.B
+    p.topLeft     := false.B
+    p.bottomRight := false.B
+    p.left        := false.B
+    p.right       := false.B
+
+    p
+  }
 }
